30373267c77281a39028b09d8393f9c2b4ee5ad7
sigmation <Ronan.d711@gmail.com>
2018-09-10 17:15:08 -0600

    Created first script. (CharacterMove)

---
+++ Dgm1610RonanDarlingGame/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	//Used For Movement
+	public int MoveSpeed;
+	public float JumpHeight = 15;
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
