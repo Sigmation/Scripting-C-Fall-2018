e4db801923a1dec7c6cfb7d81153d14d33cfa888
sigmation <Ronan.d711@gmail.com>
2018-10-15 17:18:46 -0600

    Added Enemy Movement

---
+++ Dgm1610RonanDarlingGame/Assets/Scripts/EnemyPatroll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class EnemyPatroll : MonoBehaviour {
+	//movement
+	public float MoveSpeed;
+	public bool MoveRight;
+	//walk cycle
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+	//edge check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+	
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+		//trun around at wall or edge
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-5f,6f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(5f,6f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
