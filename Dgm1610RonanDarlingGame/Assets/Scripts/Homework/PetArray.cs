c9059b2c3fb77e44dc3331e75b5a08b664812863
sigmation <Ronan.d711@gmail.com>
2018-11-07 16:20:54 -0700

    Added pet array

---
+++ Dgm1610RonanDarlingGame/Assets/Scripts/Homework/PetArray.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetArray : MonoBehaviour {
+	public string[] Pets;
+
+	// Use this for initialization
+	void Start () {
+		Pets = new string[6];
+
+		Pets[0] = "Dog";
+		Pets[1] = "Cat";
+		Pets[2] = "Bear";
+		Pets[3] = "Fox";
+		Pets[4] = "Lion";
+		Pets[5] = "Mouse";
+
+		foreach(string Animal in Pets){
+			print("I have a pet " + Animal);
+		}
+	
+
+	}
+}
